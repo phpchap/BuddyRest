6b0d22710d690709ef4cb8d99655328d57cdb7cb
Justen Doherty <phpchap@gmail.com>
2012-05-07 19:38:55 +0100

    added submodule shell script

---
+++ update_submodules.sh
@@ -0,0 +1,17 @@
+echo "initialise submodules"
+git submodule init
+echo "update submodules"
+git submodule update
+#echo "change into buddypress directory"
+#cd src/wordpress/wp-content/plugins/buddypress
+#echo "switch to master branch"
+#git checkout master
+#echo "get latest changes"
+#git pull
+#echo "back to root.."
+#cd ../../../../..
+#echo "add buddypress to main branch"
+#git add src/wordpress/wp-content/plugins/buddypress
+#git push
+#echo "change into restler directory"
+#cd src/api/lib/Restler
